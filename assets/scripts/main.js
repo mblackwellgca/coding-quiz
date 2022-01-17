f878fd47f2e20eb2ce6326a5df393d60eafb05e0
Mikki <mblackwell@georgiacyber.org>
2022-01-17 17:52:17 -0500

    added comments to main js with outline ideas for vars and functions

---
+++ assets/scripts/main.js
@@ -0,0 +1,39 @@
+
+// main vars
+var timekeeper = document.querySelector("nav");
+var startbutton = document.querySelector('#button');
+var question = [""];
+var answers = [""];
+var timeLeft = 85;
+
+
+// button
+startbutton.addEventListener("click", quizstart);
+
+
+// runs the quiz
+function quizstart() {
+    // add timer start function
+    // add question function
+    // add game end
+    
+}
+
+// timer function
+    //xx amount of time
+    //subtract 5 seconds for wrong answers
+    //end of timer and quiz alert
+
+// question function
+    // 5 questions
+    // auto move to next question
+    // record answers
+    // end quiz alert
+    // high scores
+    // alert correct/incorrect
+
+// game end
+    // save initials 
+    // save score
+    // show scores
+    // high scores record
