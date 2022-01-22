62acf02be4ff3d18cb8cf2a4fc4701613b334503
Mikki <mblackwell@georgiacyber.org>
2022-01-22 15:33:40 -0500

    adding questions js file with initial 5 questions and answers

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,10 +1,10 @@
 
 // main vars
 var timekeeper = document.querySelector("nav");
-var startbutton = document.querySelector('#button');
-var question = [""];
-var answers = [""];
-var timeLeft = 78;
+var startbutton = document.querySelector("#button");
+var questionsEl = document.querySelector["#questions"];
+var answersEl = document.querySelector["#answers"];
+var timeLeft = 5;
 
 
 // button
@@ -26,7 +26,7 @@ function countdown(){
     var timeInterval = setInterval(function () {
         if (timeLeft >1) {
             timekeeper.textContent = timeLeft + ' seconds remaining';
-            timeLeft --;
+            timeLeft -- ;
         } else {
             timekeeper.textContent = 'GAME OVER';
             clearInterval(timeInterval);
