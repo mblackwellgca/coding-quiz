528ea106da7b94d2fc657edb182ecd0a810a7571
Mikki <mblackwell@georgiacyber.org>
2022-01-23 09:55:19 -0500

    changed index and css coding as what was written before just was not working. Adding initial changes

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,50 +1,60 @@
-
-// main vars
+// vars for main Document Object Model or DOM elements
 var timekeeper = document.querySelector("nav");
 var startbutton = document.querySelector("#button");
 var questionsEl = document.querySelector["#questions"];
 var answersEl = document.querySelector["#answers"];
 var timeLeft = 5;
 
+// quiz vars
+var currentQuestionIndex = 0;
+var time = questionsEl.length * 15;
+var timerId;
 
-// button
-startbutton.addEventListener("click", quizstart);
-
+startbutton.addEventListener('click', quizstart); 
+    
 
 // runs the quiz
 function quizstart() {
-    countdown();
-    // add timer start function
-    // add question function
-    // add game end
-    
-    
-}
-
+        countdown();
+        questiontime();
+    }
+       
 // timer function
-function countdown(){
+function countdown() {
     var timeInterval = setInterval(function () {
         if (timeLeft >1) {
             timekeeper.textContent = timeLeft + ' seconds remaining';
             timeLeft -- ;
+        } else if (timeLeft ===1) {
+            timekeeper.textContent = timeLeft + ' second remaining';
+            timeLeft--;
         } else {
             timekeeper.textContent = 'GAME OVER';
             clearInterval(timeInterval);
             window.alert("GAME OVER");
         }
-    },1000);
+    }, 1000);
 }
-    //xx amount of time
-    //subtract 5 seconds for wrong answers
-    //end of timer and quiz alert
 
-// question function
-    // 5 questions
-    // auto move to next question
-    // record answers
-    // end quiz alert
-    // high scores
-    // alert correct/incorrect
+// question function. Click answer to either generate a new question or if it is the last question, end quiz.
+function questiontime() {
+    // check for correct answer
+    if (this.value !== questionsEl[currentQuestionIndex].answer) {
+        // time penalty
+        time -= 15;
+    if (time < 0) {
+        time = 0;
+        }
+    }
+
+    // display new time on page
+    timeLeft.textContent = time;
+
+
+
+
+}
+  
 
 // game end
     // save initials 
